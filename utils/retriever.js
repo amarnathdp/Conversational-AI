c349536783974f9a792f6aa3ca28bc662d7552ac
amarnathdp <118151760+amarnathdp@users.noreply.github.com>
2026-06-15 17:19:46 +0530

    Refactor retriever.js to use updated embeddings model
    Updated embeddings model to 'gemini-embedding-001' with 3072 dimensions and modified comments for clarity.

--- utils/retriever.js
+++ utils/retriever.js
@@ -6,6 +6,8 @@ import { TaskType } from "@google/generative-ai";
 
 import { MemoryVectorStore } from "langchain/vectorstores/memory";
 
+// Step - 1
+
 // As it is intended for demos, We're using In-memory
 
 // Database and embedding
@@ -18,11 +20,14 @@ const splitter = new RecursiveCharacterTextSplitter({
   separators: ['\n\n', '\n', ' ', ''] // default setting
 })
 
-const output = await splitter.createDocuments([text])
+const output = await splitter.createDocuments([text]);
+
+// The GoogleGenerativeAIEmbeddings class with the gemini-embedding-001 model in LangChain.js is restricted to the default output dimension of 3072. 
+// 768 dimensions  // 3072 ?? Google recommends 3072, 1536, or 768 dimensions for quality and efficiency.
 
 const embeddings = new GoogleGenerativeAIEmbeddings({
-  apiKey: "AIzaSyACT8KjqrkOD0HGt_eqLgkzK-PTaz_ZsKQ",
-  model: "embedding-001", // 768 dimensions
+  apiKey: "AIzaSyB61kdcrD576F2m01rh8eq7PPeDAT9oI2U",
+  model: "gemini-embedding-001", // 3072 dimensions
   taskType: TaskType.RETRIEVAL_DOCUMENT,
   title: "Document title",
 });
